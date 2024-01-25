1b9729821b6791d3ee1257c9f2c00175c2b98078
Jeff Tipps <jeff.tipps@ni.com>
2024-01-25 09:28:30 -0600

    making functions

--- draw_triangles.py
+++ draw_triangles.py
@@ -1,44 +1,36 @@
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
+def draw_triangle(points, ax=None):
+    if ax is None:
+        ax = plt.gca()
+    else:
+        fig, ax = plt.subplots()
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
 
-ax.set_xlabel('x')
-ax.set_ylabel('y')
+    patch = plt.Polygon(points)
+    ax.add_patch(patch)
 
-patch = plt.Polygon([
+    for pt in points:
+        x, y = pt
+        ax.text(x, y, '({}, {})'.format(x, y))
+
+draw_triangle([
     (0.2, 0.2),
     (0.2, 0.6),
     (0.4, 0.4)
 ])
 
-ax.add_patch(patch)
-
-ax.text(0.2, 0.4, '(0.2, 0.4)')
-ax.text(0.2, 0.6, '(0.2, 0.6)')
-ax.text(0.2, 0.4, '(0.2, 0.4)')
-
-patch = plt.Polygon([
+draw_triangle([
     (0.6, 0.8),
     (0.8, 0.8),
     (0.5, 0.5)
 ])
 
-ax.add_patch(patch)
-
-ax.text(0.6, 0.8, '(0.6, 0.8)')
-ax.text(0.8, 0.8, '(0.8, 0.8)')
-ax.text(0.5, 0.5, '(0.5, 0.5)')
-
-patch = plt.Polygon([
+draw_triangle([
     (0.6, 0.1),
     (0.7, 0.3),
     (0.9, 0.2)
 ])
-
-ax.add_patch(patch)
-
-ax.text(0.6, 0.1, '(0.6, 0.1)')
-ax.text(0.7, 0.3, '(0.7, 0.3)')
-ax.text(0.9, 0.2, '(0.9, 0.2)')
 
 plt.show()
