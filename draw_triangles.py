37eec4c66e8dc3353c2b2ea14f24c4cc672ae276
Jeff Tipps <jeff.tipps@ni.com>
2024-01-25 09:39:18 -0600

    made first package

--- draw_triangles.py
+++ draw_triangles.py
@@ -1,37 +1,21 @@
-import matplotlib.pyplot as plt
+from geometry import graphics
 
-def draw_triangle(points, ax=None):
-    if ax is None:
-        ax = plt.gca()
-    else:
-        fig, ax = plt.subplots()
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
+graphics.draw_triangle([
+    (0.2, 0.2),
+    (0.2, 0.6),
+    (0.4, 0.4)
+])
 
-    patch = plt.Polygon(points)
-    ax.add_patch(patch)
+graphics.draw_triangle([
+    (0.6, 0.8),
+    (0.8, 0.8),
+    (0.5, 0.5)
+])
 
-    for pt in points:
-        x, y = pt
-        ax.text(x, y, '({}, {})'.format(x, y))
+graphics.draw_triangle([
+    (0.6, 0.1),
+    (0.7, 0.3),
+    (0.9, 0.2)
+])
 
-if __name__ == "__main__":
-    draw_triangle([
-        (0.2, 0.2),
-        (0.2, 0.6),
-        (0.4, 0.4)
-    ])
-
-    draw_triangle([
-        (0.6, 0.8),
-        (0.8, 0.8),
-        (0.5, 0.5)
-    ])
-
-    draw_triangle([
-        (0.6, 0.1),
-        (0.7, 0.3),
-        (0.9, 0.2)
-    ])
-
-    plt.show()
+graphics.show()
