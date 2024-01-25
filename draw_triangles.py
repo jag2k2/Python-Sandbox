933a035bac0c9736fc40f7bd61466b4043f86538
Jeff Tipps <jeff.tipps@ni.com>
2024-01-25 09:29:53 -0600

    adding __main__

--- draw_triangles.py
+++ draw_triangles.py
@@ -15,22 +15,23 @@ def draw_triangle(points, ax=None):
         x, y = pt
         ax.text(x, y, '({}, {})'.format(x, y))
 
-draw_triangle([
-    (0.2, 0.2),
-    (0.2, 0.6),
-    (0.4, 0.4)
-])
+if __name__ == "__main__":
+    draw_triangle([
+        (0.2, 0.2),
+        (0.2, 0.6),
+        (0.4, 0.4)
+    ])
 
-draw_triangle([
-    (0.6, 0.8),
-    (0.8, 0.8),
-    (0.5, 0.5)
-])
+    draw_triangle([
+        (0.6, 0.8),
+        (0.8, 0.8),
+        (0.5, 0.5)
+    ])
 
-draw_triangle([
-    (0.6, 0.1),
-    (0.7, 0.3),
-    (0.9, 0.2)
-])
+    draw_triangle([
+        (0.6, 0.1),
+        (0.7, 0.3),
+        (0.9, 0.2)
+    ])
 
-plt.show()
+    plt.show()
