580a3de80b0aab04c0ff0947559c3b54096442f7
Jeff Tipps <jeff.tipps@ni.com>
2024-02-17 13:26:22 -0600

    adding triangledraw as a package

--- example/example.py
+++ example/example.py
@@ -1,6 +1,9 @@
 from squaremaker.square import indentify, areaCalculator, volCalculator
+from triangledraw.draw.triplot import make_plot
 
 side = 2
 msg = indentify(side)
 print(msg, "My area is", areaCalculator(side))
 print(msg, "My volume is", volCalculator(side))
+
+make_plot()
---
+++ triangledraw/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='triangledraw',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib>=3.0.0'
+    ]
+)
